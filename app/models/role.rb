bf104c11b94162da3908fd0d83a8351496a92624
kiat-mao <kiat.mao@gmail.com>
2019-03-19 17:33:12 +0800

    fix role bug in ROLE[]

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,7 +4,7 @@ class Role < ActiveRecord::Base
 
   validates_presence_of :user_id, :unit_id, :role, :message => '不能为空字符'
   validates_uniqueness_of :user_id, scope: [:unit_id, :role], :message => '该角色在该仓库中角色已存在'
-  ROLE = { deviceadmin: '设备管理员', inventoryadmin: '盘点员', accountant: '财务', sgsadmin: '市公司管理员', '机构管理员', user: '普通用户'}
+  ROLE = { deviceadmin: '设备管理员', inventoryadmin: '盘点员', accountant: '财务', sgsadmin: '市公司管理员', unitadmin: '机构管理员', user: '普通用户'}
   ROLE_SGS = { deviceadmin: '设备管理员', inventoryadmin: '盘点员', accountant: '财务', sgsadmin: '市公司管理员'}
   ROLE1_SB = { deviceadmin: '设备管理员', inventoryadmin: '盘点员', sgsadmin: '市公司管理员'}
   ROLE1_CW = { accountant: '财务', inventoryadmin: '盘点员', sgsadmin: '市公司管理员'}
